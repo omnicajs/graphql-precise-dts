0ec200e263941c3ec1cd0f416e88534cd7d51d3e
Tatyana <komolyh@retailcrm.ru>
2026-04-08 11:04:13 +0300

    test: Vitest typecheck globs were separated

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,8 +8,13 @@ import basic from './vite.config.basic'
 export default mergeConfig(basic, defineConfig({
     test: {
         include: [
-            '**/*.{test,test-d}.?(c|m)[jt]s?(x)',
+            '**/*.test.?(c|m)[jt]s?(x)',
         ],
+        typecheck: {
+            include: [
+                '**/*.test-d.?(c|m)[jt]s?(x)',
+            ],
+        },
         coverage: {
             provider: 'v8',
             reportsDirectory: './coverage',
